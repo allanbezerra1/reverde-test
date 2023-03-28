57d1157d0fb00e9073999dc5e922344ed7efe614
Allan Bezerra <allanbezerra95@gmail.com>
2023-03-28 01:22:46 -0300

    feat: criação de páginas para interação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { GlobalStateProvider } from "../context/common-state";
+import "../styles/globals.css";
+import "../styles/login.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStateProvider>
+        <Component {...pageProps} />
+      </GlobalStateProvider>
+    </>
+  );
 }
+
+export default MyApp;
